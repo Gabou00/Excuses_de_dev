8cfe4fdf0bbfae2aa0477b1266198342b9f8a5e9
Gabi <gabriel.delachat@gmail.com>
2022-09-05 14:59:45 +0200

    ajout de la fonction setTimeout

--- src/components/assets/button.tsx
+++ src/components/assets/button.tsx
@@ -9,14 +9,17 @@ function Button() {
   const [quote, setQuote] = useState();
 
   useEffect(() => {
+     // Fonction setTimeout pour l'Illusion de labeur
+    setTimeout(() => {
     isSending &&
-      fetch(url)
-        .then((response) => response.json())
-        .then(data => (data[Math.floor(Math.random() * data.length)]["message "]))
-        .then((quote: any) => (setQuote(quote)))
-        .then(() => setIsSending(false))
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => (data[Math.floor(Math.random() * data.length)]["message "]))
+    .then((quote: any) => (setQuote(quote)))
+    .then(() => setIsSending(false))
+  }, Math.floor(Math.random() * 4000 + 1000));
   }, [isSending]);
-
+  
 
   return (
     <div>
